f64e30fb11ad98e98285471b176365852d47658e
jan.custovic <jan.custovic@gmail.com>
2014-12-11 16:13:48 +0100

    - upgrade to spring 1.2.0 spring boot and removed custom email config

--- sendish-api/src/main/java/com/sendish/api/SendishApiApplication.java
+++ sendish-api/src/main/java/com/sendish/api/SendishApiApplication.java
@@ -3,19 +3,15 @@ package com.sendish.api;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
+@SpringBootApplication
 @EntityScan(basePackages = "com.sendish.repository.model.jpa")
 @EnableJpaRepositories(basePackages = "com.sendish.repository")
 @EnableTransactionManagement
