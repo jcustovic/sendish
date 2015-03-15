4f4aabc77c7fb29387d383511a7361cfddc47dba
jan.custovic <jan.custovic@gmail.com>
2015-03-15 15:33:16 +0100

    - fix

--- sendish-api/src/main/java/com/sendish/api/web/controller/validator/UserRegistrationValidator.java
+++ sendish-api/src/main/java/com/sendish/api/web/controller/validator/UserRegistrationValidator.java
@@ -1,22 +1,20 @@
 package com.sendish.api.web.controller.validator;
 
-import com.sendish.api.dto.UserRegistration;
-import com.sendish.repository.UserRepository;
-import com.sendish.repository.model.jpa.User;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
+import com.sendish.api.dto.UserRegistration;
+import com.sendish.api.service.impl.UserServiceImpl;
+import com.sendish.repository.model.jpa.User;
+
 @Component
-@Transactional(readOnly = true)
 public class UserRegistrationValidator implements Validator {
 	
 	@Autowired
-	private UserRepository userRepository;
+	private UserServiceImpl userService;
 
     @Override
     public boolean supports(Class<?> clazz) {
@@ -32,7 +30,7 @@ public class UserRegistrationValidator implements Validator {
     		}
     	}
     	
-    	User user = userRepository.findByUsernameIgnoreCaseOrEmailIgnoreCase(userReg.getEmail(), userReg.getEmail());
+    	User user = userService.findByUsernameIgnoreCaseOrEmailIgnoreCase(userReg.getEmail(), userReg.getEmail());
     	if (user != null) {
     		errors.rejectValue("email", null, "Email already exists");
     	}
