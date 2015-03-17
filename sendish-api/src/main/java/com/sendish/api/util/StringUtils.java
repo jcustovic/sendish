cec1c7931ea63a9cb6cc3e8a7cdaeff20bfdcaaa
janc <jan.custovic@zooplus.com>
2015-03-17 10:15:10 +0100

    - fix

--- sendish-api/src/main/java/com/sendish/api/util/StringUtils.java
+++ sendish-api/src/main/java/com/sendish/api/util/StringUtils.java
@@ -9,9 +9,9 @@ public class StringUtils {
     public static String trim(String string, int maxLength, String suffix) {
         if (string.length() > maxLength) {
             if (suffix == null) {
-                return org.apache.commons.lang3.StringUtils.left(string, maxLength - suffix.length()) + suffix;
-            } else {
                 return org.apache.commons.lang3.StringUtils.left(string, maxLength);
+            } else {
+                return org.apache.commons.lang3.StringUtils.left(string, maxLength - suffix.length()) + suffix;
             }
         } else {
             return string;
