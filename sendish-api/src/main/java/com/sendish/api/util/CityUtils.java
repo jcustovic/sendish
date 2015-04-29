e1cb29684290309ceef9fa239bacff812198d8f6
jan.custovic <jan.custovic@gmail.com>
2015-04-29 21:09:45 +0200

    - location name fix

--- sendish-api/src/main/java/com/sendish/api/util/CityUtils.java
+++ sendish-api/src/main/java/com/sendish/api/util/CityUtils.java
@@ -17,7 +17,7 @@ public class CityUtils {
     public static final String getLocationName(City city, int trimLength) {
         int cityLength = city.getName().length();
         if (cityLength > trimLength - 5) {
-            return StringUtils.trim(city.getName(), trimLength, "..., "); // Because of iOS, we have to have ', '
+            return StringUtils.trim(getLocationName(city), trimLength, "...,"); // Because of iOS, we have to have ','
         } else if (cityLength == trimLength - 5) {
             return city.getName() + ", " + city.getCountry().getIso();
         //} else if (cityLength > trimLength - 8) {
