ca5d34c7721c00e064e168cba1ddec868a36d48b
jan.custovic <jan.custovic@gmail.com>
2015-04-29 21:27:45 +0200

    - location name improved in some cases where city length is large to use country iso

--- sendish-api/src/main/java/com/sendish/api/util/CityUtils.java
+++ sendish-api/src/main/java/com/sendish/api/util/CityUtils.java
@@ -15,13 +15,11 @@ public class CityUtils {
     }
 
     public static final String getLocationName(City city, int trimLength) {
-        int cityLength = city.getName().length();
+    	int cityLength = city.getName().length();
         if (cityLength > trimLength - 5) {
             return StringUtils.trim(getLocationName(city), trimLength, "...,"); // Because of iOS, we have to have ','
-        } else if (cityLength == trimLength - 5) {
+        } else if (cityLength > trimLength - 8) {
             return city.getName() + ", " + city.getCountry().getIso();
-        //} else if (cityLength > trimLength - 8) {
-        //    return city.getName() + ", " + city.getCountry().getIso3();
         } else {
             return StringUtils.trim(getLocationName(city), trimLength, "...");
         }
